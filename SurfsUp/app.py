5e324e3ae4124eabc43bea862ec3b50562f66c20
Ivan Villa <itanalytics21@gmail.com>
2023-04-01 19:52:03 -0700

    Flask App Updated

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -45,8 +45,8 @@ def welcome():
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
     )
 
 @app.route("/api/v1.0/precipitation")
@@ -58,6 +58,9 @@ def precipitation():
     prior_year = dt.date(recent_date[0],recent_date[1], recent_date[2]) - dt.timedelta(days=365)
     # Perform a query to retrieve the data and precipitation scores
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= prior_year).all()
+    
+    session.close()
+    
     prcp_dict = dict(results)
 
     return jsonify(prcp_dict)
@@ -65,6 +68,9 @@ def precipitation():
 @app.route("/api/v1.0/stations")
 def stations():
     results = session.query(Station.id, Station.station).all()
+
+    session.close()
+
     station_list = list(np.ravel(results))
 
     return jsonify(station_list)
@@ -80,46 +86,33 @@ def tobs():
     .filter(Station.station == Measurement.station)\
     .filter(Station.id == active_station).all()
 
+    session.close()
+
     temp_list = list(np.ravel(temps))
 
     return jsonify(temp_list)
 
 
 @app.route("/api/v1.0/<start>")
-def start(start_date):
-
-    recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
-    # Calculate the date one year from the last date in data set.
-    recent_date = recent_date.split('-')
-    recent_date = [int(recent_date[i]) for i in range(0,len(recent_date))]
-    
-    if dt.date(recent_date[0],recent_date[1], recent_date[2]) >= start_date:
-
-        temp_stats = session.query(func.min(Measurement.tobs),func.round(func.avg(Measurement.tobs),1),func.max(Measurement.tobs))\
-        .filter(Measurement.date >= start_date).all()
-        min_temp = temp_stats[0][0]
-        mean_temp = temp_stats[0][1]
-        max_temp = temp_stats [0][2]
-        return jsonify({'Results':f'Beginning on {start_date}, the minimum temperature was {min_temp}, the average temperature was {mean_temp}, and the maximum temperature was {max_temp}'})
-    
-    return jsonify({"error": f"Start date not found"}), 404
-
-
 @app.route("/api/v1.0/<start>/<end>")
-def start_end(start_date, end_date):
+def temp_stats(start=None, end=None):
+
     recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()[0]
     # Calculate the date one year from the last date in data set.
     recent_date = recent_date.split('-')
     recent_date = [int(recent_date[i]) for i in range(0,len(recent_date))]
     
-    if dt.date(recent_date[0],recent_date[1], recent_date[2]) >= start_date:
-        temp_stats = session.query(func.min(Measurement.tobs),func.round(func.avg(Measurement.tobs),1),func.max(Measurement.tobs))\
-        .filter(Measurement.date >= start_date)\
-        .filter(Measurement.date <= end_date).all()
-        min_temp = temp_stats[0][0]
-        mean_temp = temp_stats[0][1]
-        max_temp = temp_stats [0][2]
-        return jsonify({'Results':f'Between on {start_date} & {end_date}, the minimum temperature was {min_temp}, the average temperature was {mean_temp}, and the maximum temperature was {max_temp}'}) 
+    if dt.date(recent_date[0],recent_date[1], recent_date[2]) >= start:
+
+        stats = session.query(func.min(Measurement.tobs),func.round(func.avg(Measurement.tobs),1),func.max(Measurement.tobs))\
+        .filter(Measurement.date >= start)\
+        .filter(Measurement.date <= end).all()
+
+        session.close()
+
+        temps = list(np.ravel(stats))
+        
+        return jsonify(temps)
     
     return jsonify({"error": f"Start date not found"}), 404
 
